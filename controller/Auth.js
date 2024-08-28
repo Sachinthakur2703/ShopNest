03c067ec3e9ae9b85bec666c0f38146c884ec9c5
Sachinthakur2703 <165869162+Sachinthakur2703@users.noreply.github.com>
2024-08-28 11:24:30 +0530

    Add files via upload

--- controller/Auth.js
+++ controller/Auth.js
@@ -70,7 +70,7 @@ exports.checkAuth=async(req,res)=>{
        await user.save()
 
        //also set token in email
-    const resetPageLink ="/reset-password?token="+token+"&email="+email;
+    const resetPageLink ="https://shop-nest-blue.vercel.app/reset-password?token="+token+"&email="+email;
     const subject="reset password for e-commerce";
     const html=`<p>Click<a href='${resetPageLink}'>here </a> to Reset Password</p>`
 
